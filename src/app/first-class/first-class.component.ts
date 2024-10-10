b0d97e254ab46f79abfa3f67ebab942c044a9214
simplysekinah <akinolasekinat123@gmail.com>
2024-10-10 13:43:10 +0100

    first commit

---
+++ src/app/first-class/first-class.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-class',
+  standalone: true,
+  imports: [],
+  templateUrl: './first-class.component.html',
+  styleUrl: './first-class.component.css'
+})
+export class FirstClassComponent {
+public name = "Sekinat"
+}
